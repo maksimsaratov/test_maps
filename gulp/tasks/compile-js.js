29bec32cfcf698b7ace4ad891233e1c57085599f
Maksim Saratov <maksim.saratov@yahoo.com>
2015-08-17 16:33:03 +0300

    added uglify for js code

--- gulp/tasks/compile-js.js
+++ gulp/tasks/compile-js.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var connect = require('gulp-connect');
 
 gulp.task('compile-js', function() {
     return gulp.src(['./src/www/app/**/*.js'])
         .pipe(concat('app.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('./dist/js'))
         .pipe(connect.reload());
 });
